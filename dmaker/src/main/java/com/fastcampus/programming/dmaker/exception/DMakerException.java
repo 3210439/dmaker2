d8d2be7569e696fa0505f58b90b0ab07e41588e9
kimde <kimdeiz@naver.com>
2021-11-28 11:53:53 +0900

    validation 기능을 추가하였습니다. 데이터의 값이 특정 조건을 만족하지 않는지 체크하고 또는 해당 아이디 값이 이미 존재하여 insert를 하면 안되는 상황인지를 체크합니다.

---
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/exception/DMakerException.java
@@ -0,0 +1,17 @@
+package com.fastcampus.programming.dmaker.exception;
+
+public class DMakerException extends RuntimeException{
+    private DMakerErrorCode dMakerErrorCode;
+    private String detailMessage;
+
+    public DMakerException(DMakerErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.dMakerErrorCode = errorCode;
+        this.detailMessage = errorCode.getMessage();
+    }
+    public DMakerException(DMakerErrorCode errorCode, String detailMessage){
+        super(errorCode.getMessage());
+        this.dMakerErrorCode = errorCode;
+        this.detailMessage = detailMessage;
+    }
+}
